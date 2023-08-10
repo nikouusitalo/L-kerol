93c019c16d4cceb1e08fd555996c41238ed8b8bf
Niko <niko.u@hotmail.fi>
2023-08-10 21:32:25 +0300

    prisma

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/',(req:any, res:any, next:any) => {
-    res.send("Root route is working");
+    res.send("Roooooooooooot route is working");
 });
 app.listen(3000,() => {
     console.log("Server started on port 3000");
